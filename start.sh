dc92d5dcffcedb8597be1de2569bbe0b9fe14af5
thascc <thascc1024@gmail.com>
2019-10-11 19:19:18 +0800

    fix

--- start.sh
+++ start.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-if [ ! -f /www/server/panel/data/notfirst ]; then
+if [ ! -f /www/server/panel/data/default.db ]; then
 # 根据环境变量修改用户名和密码
 sh -c  '/bin/echo -e "$PASSWORD" | /bin/bt 5' && sh -c  '/bin/echo -e "$USERNAME" | /bin/bt 6'
-echo '此文件用于表示已经设置过用户名和密码了' > /www/server/panel/data/notfirst
 fi
 
-if [ ! -f /www/server/nginx/conf/notfirst ]; then
+if [ ! -f /www/server/nginx/conf/nginx.conf ]; then
 ## 首次启动时补充由于映射volume丢失的文件
-cp -r /thas/nginx/conf /www/server/nginx/
-echo '此文件用于表示非首次启动' > /www/server/nginx/conf/notfirst
+cp -nr /thas/nginx/conf /www/server/nginx/
 fi
 
 # 启动宝塔面板
